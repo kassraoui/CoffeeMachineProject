e6667395738a6939f85a3e255648a0d0480c344b
Mehdi Kassraoui <mehdikassraoui@gmail.com>
2020-03-07 14:29:09 +0100

    Create Projects

---
+++ CoffeeMachine.UnitTests/InterpreterTests.cs
@@ -0,0 +1,14 @@
+﻿
+using NUnit.Framework;
+
+namespace CoffeeMachine.UnitTests
+{
+    [TestFixture]
+    public class InterpreterTests
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ CoffeeMachine.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoffeeMachine.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoffeeMachine.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("144dbf85-6359-4f70-b48e-aabd9515590b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
